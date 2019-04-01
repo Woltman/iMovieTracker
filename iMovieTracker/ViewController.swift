5ec0f92da03800b9715b54db19871c1ea92922de
Thom Woltman <twoltman@pc179001.local>
2019-04-01 14:04:18 +0200

    movie list from api

--- iMovieTracker/ViewController.swift
+++ iMovieTracker/ViewController.swift
@@ -15,6 +15,32 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let url = URL(string: "https://api.themoviedb.org/3/discover/movie?api_key=08989a42239e5b70c6e742a879cc531d"){
+            let task = URLSession.shared.dataTask(with: url){ data, response, error in
+                if let receivedData = data {
+                    Swift.print("\(receivedData)")
+                    
+                    do {
+                        // Optie 1: Gebruik JSONSerialization
+                        
+                        if let json = try JSONSerialization.jsonObject(with: receivedData) as? [String:Any] {
+                            //Swift.print("\(json)")
+                            if let movies = json["results"] as? [Any] {
+                                //Swift.print("\(movies)")
+                                for movie in movies{
+                                    Swift.print(movie)
+                                }
+                            }
+                        }
+                    } catch { }
+                }
+                
+            }
+            
+            task.resume()
+        }
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
