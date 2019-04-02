3c49a8b06a8dd794e367c44b78c08bca8ca14152
Niels <Niels.e@live.nl>
2019-04-02 15:07:27 +0200

    changed tab

---
+++ iMovieTracker/SearchController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchController.swift
+//  iMovieTracker
+//
+//  Created by Niels Evenblij on 02/04/2019.
+//  Copyright © 2019 thomniels. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UITableViewController, UISearchBarDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//        if let selectedIndexPath = tableView.indexPathForSelectedRow,
+//            let destination = segue.destination as? DetailsController {
+//            destination.movie = movies[selectedIndexPath.row]
+//        }
+//    }
+}
+
+
